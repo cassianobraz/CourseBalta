d314282238c9d1ab51ad3053ef858a957658b74a
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2025-05-27 16:54:53 -0400

    feat: project repository with dapper

---
+++ 02. .NET Data Access/Blog/Program.cs
@@ -0,0 +1,48 @@
+﻿using Blog.Models;
+using Blog.Repositories;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True;";
+
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+
+            ReadUsers(connection);
+            ReadRoles(connection);
+            ReadTags(connection);
+
+            connection.Close();
+        }
+
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var users = repository.Get();
+
+            foreach (var item in users)
+                Console.WriteLine(item.Name);
+        }
+        public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new Repository<Role>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+        public static void ReadTags(SqlConnection connection)
+        {
+            var repository = new Repository<Tag>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+    }
+}
