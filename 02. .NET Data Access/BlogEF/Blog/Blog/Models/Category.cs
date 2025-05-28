117f73f5f0f02c4427d541e7906f07a60ed20bdf
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2025-05-28 17:13:08 -0400

    feat: new pagination

---
+++ 02. .NET Data Access/BlogEF/Blog/Blog/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+    public IList<Post> Posts { get; set; } = [];
+}
---
+++ 02. .NET Data Access/BlogEF/Blog/Blog/Program.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+using var context = new BlogDataContext();
+
+var posts = GetPosts(context, 0, 25);
+
+Console.WriteLine("Teste");
+
+
+static List<Post> GetPosts(BlogDataContext context, int skip, int take = 25)
+{
+    var posts = context
+        .Posts
+        .AsNoTracking()
+        .Skip(skip)
+        .Take(take)
+        .ToList();
+    return posts;
+}
