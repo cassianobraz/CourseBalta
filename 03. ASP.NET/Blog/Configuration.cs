05fc900a0e0551e4dff4681ee18a5aa189aef935
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2025-05-30 14:19:42 -0400

    feat: finish jwt

--- 03. ASP.NET/Blog/Configuration.cs
+++ 03. ASP.NET/Blog/Configuration.cs
@@ -2,5 +2,7 @@
 
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "ueLfb0F6bG27d+ON7sf30Lcm8I/cuhO8RsMTfInWm+E=";
+    public static string JwtKey = "ueLfb0F6bG27d+ON7sf30Lcm8I/cuhO8RsMTfInWm+E=";
+    public static string ApiKeyName = "api-key";
+    public static string ApiKey = "curso_api_cuhO8RsMTfInWm+E=";
 }
