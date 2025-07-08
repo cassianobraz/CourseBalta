e396093566f76431568bde8669e4a0e83308c799
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2025-07-08 10:10:07 -0400

    feat: implementation authentication

---
+++ 05. Advanced .NET Developer/modulo04/jwt/JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Authenticate/Handler.cs
@@ -0,0 +1,87 @@
+﻿using JwtStore.Core.Contexts.AccountContext.Entities;
+using JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Contracts;
+using MediatR;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate;
+
+public class Handler : IRequestHandler<Request, Response>
+{
+    private readonly IRepository _repository;
+
+    public Handler(IRepository repository) => _repository = repository;
+
+    public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+    {
+        #region 01. Valid request
+
+        try
+        {
+            var res = Specification.Ensure(request);
+            if (!res.IsValid)
+                return new Response("Request invalid", 400, res.Notifications);
+
+        }
+        catch (Exception)
+        {
+            return new Response("Not possible valid your request", 500);
+        }
+
+        #endregion
+
+        #region 02. Recover profile
+
+        User? user;
+        try
+        {
+            user = await _repository.GetUserByEmailAsync(request.Email, cancellationToken);
+            if (user is null)
+                return new Response("Profile not found", 404);
+        }
+        catch (Exception ex)
+        {
+            return new Response("Not possible recover your profile", 500);
+        }
+
+        #endregion
+
+        #region 03. Valid password
+
+        if (!user.Password.Challenge(request.Password))
+            return new Response("User or password invalid", 400);
+        #endregion
+
+        #region 04. Password Check is valid
+
+        try
+        {
+            if (!user.Email.Verification.IsActive)
+                return new Response("Acount inactive", 400);
+        }
+        catch (Exception)
+        {
+            return new Response("Not possible verify your profile", 500);
+        }
+
+        #endregion
+
+        #region 05. Return data
+
+        try
+        {
+            var data = new ResponseData
+            {
+                Id = user.Id.ToString(),
+                Name = user.Name,
+                Email = user.Email,
+                Roles = Array.Empty<string>()
+            };
+            return new Response(string.Empty, data);
+        }
+        catch
+        {
+            return new Response("Not possible get data the profile", 500);
+        }
+
+        #endregion
+    }
+}
---
+++ 05. Advanced .NET Developer/modulo04/jwt/JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Authenticate/Specification.cs
@@ -0,0 +1,14 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate;
+
+public static class Specification
+{
+    public static Contract<Notification> Ensure(Request request) =>
+        new Contract<Notification>()
+            .Requires()
+            .IsLowerThan(request.Password.Length, 40, "Password", "The password than menus fourty caracter")
+            .IsGreaterThan(request.Password.Length, 8, "Password", "The password than more eight caracter")
+            .IsEmail(request.Email, "Email", "The email is not valid");
+}
