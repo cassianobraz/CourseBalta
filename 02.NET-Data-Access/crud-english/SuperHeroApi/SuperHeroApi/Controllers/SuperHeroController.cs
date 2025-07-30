dc66969bac1b48539216d4d175a0839fd26e6c9a
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2025-07-30 16:42:24 -0400

    new project SuperHeroes with EF

---
+++ 02.NET-Data-Access/crud-english/SuperHeroApi/SuperHeroApi/Controllers/SuperHeroController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperHeroApi.Data;
+using SuperHeroApi.Entities;
+
+namespace SuperHeroApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class SuperHeroController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public SuperHeroController(DataContext context) => _context = context;
+    
+    [HttpGet]
+    public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+    {
+        var heroes = await _context.SuperHeroes.ToListAsync();
+
+        return Ok(heroes);
+    }
+}
