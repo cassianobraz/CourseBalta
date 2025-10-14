f714dfa844173f8abf48406e06eb84681fb9f0ec
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2025-10-14 08:46:54 -0400

    feat: new project and migration pattern

---
+++ backend/RepositoryPattern/RepositoryStore/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using RepositoryStore.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var ConnectionStrings = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(ConnectionStrings);
+});
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
